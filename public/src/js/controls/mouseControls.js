d3c3550d0d16437f9a2602558d52b917f12e41e2
WilliamLennartsson <william.j.lennartsson@gmail.com>
2020-10-16 13:39:41 +0200

    mouseControl setup function

---
+++ public/src/js/controls/mouseControls.js
@@ -0,0 +1,31 @@
+import { PointerLockControls } from '../../dependencies/PointerLockControls.js'
+
+export default function createControls(scene, camera) {
+    const controls = new PointerLockControls( camera, document.body );
+    var blocker = document.getElementById( 'blocker' );
+    var instructions = document.getElementById( 'instructions' );
+
+    instructions.addEventListener( 'click', function () {
+
+        controls.lock();
+
+    }, false );
+
+    controls.addEventListener( 'lock', function () {
+
+        instructions.style.display = 'none';
+        blocker.style.display = 'none';
+
+    } );
+
+    controls.addEventListener( 'unlock', function () {
+
+        blocker.style.display = 'block';
+        instructions.style.display = '';
+
+    } );
+
+    scene.add( controls.getObject() );
+
+    return controls
+}
