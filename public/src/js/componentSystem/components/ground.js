180fe2237e708326fcca48d715d232708ecc29df
WilliamLennartsson <william.j.lennartsson@gmail.com>
2020-10-16 18:29:28 +0200

    ground component

---
+++ public/src/js/componentSystem/components/ground.js
@@ -0,0 +1,15 @@
+import * as THREE from '../../../dependencies/three.module.js'
+import Component from './component.js'
+
+export default class Ground extends Component {
+  constructor(gameObject, width, height) {
+    super(gameObject)
+    var geometry = new THREE.PlaneGeometry(width, height, 1 )
+    var material = new THREE.MeshBasicMaterial( {color: 0x228B22, side: THREE.DoubleSide} )
+    var plane = new THREE.Mesh( geometry, material )
+    plane.rotation.x = Math.PI / 2
+    console.log("plane", plane)
+    gameObject.transform.add(plane)
+    console.log("Ground gameobject", gameObject.transform)
+  }
+}
