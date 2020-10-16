7a293fd4c27accfcc12a7f56291a4684432cc196
WilliamLennartsson <william.j.lennartsson@gmail.com>
2020-10-16 16:32:33 +0200

    cleanup

--- public/src/js/controls/mouseControls.js
+++ public/src/js/controls/mouseControls.js
@@ -1,31 +1,28 @@
 import { PointerLockControls } from '../../dependencies/PointerLockControls.js'
 
-export default function createControls(scene, camera) {
-    const controls = new PointerLockControls( camera, document.body );
-    var blocker = document.getElementById( 'blocker' );
-    var instructions = document.getElementById( 'instructions' );
+export default class MouseControlManager {
+
+  constructor(scene, camera) {
+
+    const controls = new PointerLockControls( camera, document.body )
+    var blocker = document.getElementById( 'blocker' )
+    var instructions = document.getElementById( 'instructions' )
 
     instructions.addEventListener( 'click', function () {
-
-        controls.lock();
-
-    }, false );
+        controls.lock()
+    }, false )
 
     controls.addEventListener( 'lock', function () {
-
-        instructions.style.display = 'none';
-        blocker.style.display = 'none';
-
-    } );
+        instructions.style.display = 'none'
+        blocker.style.display = 'none'
+    } )
 
     controls.addEventListener( 'unlock', function () {
+        blocker.style.display = 'block'
+        instructions.style.display = ''
 
-        blocker.style.display = 'block';
-        instructions.style.display = '';
-
-    } );
-
-    scene.add( controls.getObject() );
-
-    return controls
+    } )
+  
+   scene.add( controls.getObject() ) 
+  }
 }
