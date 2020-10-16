ed9b559ebdf56852990b10f46c465fc39e5141d1
WilliamLennartsson <william.j.lennartsson@gmail.com>
2020-10-16 13:06:06 +0200

    loadingManager for html element

---
+++ public/src/js/loadingManager.js
@@ -0,0 +1,20 @@
+
+import * as THREE from '../dependencies/three.module.js'
+
+const manager = new THREE.LoadingManager();
+
+function events(onLoadCallback) {
+    const progressbarElem = document.querySelector('#progressbar');
+    manager.onProgress = (url, itemsLoaded, itemsTotal) => {
+        progressbarElem.style.width = `${itemsLoaded / itemsTotal * 100 | 0}%`;
+    };
+    manager.onLoad = () => {
+        // prepModelsAndAnimations()
+        onLoadCallback()
+    }
+}
+
+export default {
+    manager, 
+    events
+}
